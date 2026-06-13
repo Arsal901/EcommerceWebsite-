6261710af1eed1064c0fc7690825f3d7a94bff0b
Arsal Saifi <arshusaifi3@gmail.com>
2026-06-13 16:12:55 +0530

    Add files via upload

---
+++ PaymentButton.jsx
@@ -0,0 +1,58 @@
+import axios from "axios";
+
+function PaymentButton() {
+
+  const handlePayment = async () => { 
+    try {
+
+      const response = await axios.post(
+        // "http://localhost:5000/create-order", 
+        "https://ecommerce-website-x8ql.vercel.app/create-order", 
+        {
+          amount: 499, 
+        }
+      );
+
+      const order = response.data;
+
+      const options = {
+        key: "rzp_test_T12oAdDJX9pN7o",
+
+        amount: order.amount,
+
+        currency: order.currency,
+
+        order_id: order.id,
+
+        name: "My Store",
+
+        description: "Product Purchase",
+
+        handler: function (response) {
+          alert("Payment Successful");
+
+          console.log(response);
+        },
+
+        theme: {
+          color: "#3399cc",
+        },
+      };
+
+      const razorpay = new window.Razorpay(options);
+
+      razorpay.open();
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <button onClick={handlePayment}> 
+      Pay ₹499
+    </button>
+  );
+}
+
+export default PaymentButton; 
